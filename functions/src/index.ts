1d1587306b5be675cfc9da6508134209af5f4416
Dongyu Wei <dongyu.wei@west.cmu.edu>
2018-04-19 00:35:38 -0700

    First version: store uploaded image; get download url o of image; send notification.

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -31,8 +31,8 @@ const sendNotification = (uid, url) => {
     // See documentation on defining a message payload.
     let message = {
         data: {
-            score: '850',
-            time: '2:45'
+            url: url,
+            time: '00:00'
         },
         notification:{
             title: 'Danger!',
@@ -52,90 +52,7 @@ const sendNotification = (uid, url) => {
         });
 };
 
-
 exports.upload = functions.https.onRequest((req, res) => {
-    if (req.method === 'POST') {
-
-        console.log('req: ', req);
-
-        console.log('req headers: ', req.headers);
-        // let headers_json = JSON.parse(req.headers);
-        // uid_global = headers_json.uid;
-        // console.log('uid from headers', uid_global);
-        const busboy = new Busboy({ headers: req.headers });
-        // This object will accumulate all the uploaded files, keyed by their name
-        const uploads = {};
-
-        // This callback will be invoked for each file uploaded
-        busboy.on('file', (fieldname, file, filename, encoding, mimetype) => {
-
-            // console.log('the file is ', file);
-            filename_global = filename;
-            console.log(`File [${fieldname}] filename: ${filename}, encoding: ${encoding}, mimetype: ${mimetype}`);
-            // Note that os.tmpdir() is an in-memory file system, so should only
-            // be used for files small enough to fit in memory.
-            const filepath = path.join(os.tmpdir(), filename);
-            uploads[fieldname] = { file: filepath };
-            console.log(`Saving '${fieldname}' to ${filepath}`);
-            file.pipe(fs.createWriteStream(filepath));
-
-        });
-
-        // This callback will be invoked after all uploaded files are saved.
-        busboy.on('finish', () => {
-            console.log('finished: ', uploads);
-            fs.readdir('/tmp/', (err, files) => {
-                if(!err) {
-                    files.forEach(file => {
-                        console.log('file under tmp: ',file);
-                    });
-                }else{
-                    console.log(err);
-                }
-            });
-
-            for (const name in uploads) {
-                const upload = uploads[name];
-                const file = upload.file;
-                res.write(`${file}\n`);
-                // fs.unlinkSync(file);
-
-                const options = {
-                    destination: '/images/'+ uid_global +'/' + filename_global
-                };
-
-                bucket.upload(file, options).then(data => {
-                    fs.unlinkSync(file);
-
-                    const file_uploaded = bucket.file('/images/'+ uid_global +'/' + filename_global);
-                    sendNotification(uid_global, 'url goes here');
-                    return file_uploaded.getSignedUrl({
-                        action: 'read',
-                        expires: '03-09-2491'
-                    })
-                }).then(url => {
-                    console.log(url);
-                }).catch(error => {
-                    console.error(error);
-                });
-
-            }
-
-
-
-            res.end();
-        });
-
-        // The raw bytes of the upload will be in req.rawBody.  Send it to busboy, and get
-        // a callback when it's finished.
-        busboy.end(req.rawBody);
-    } else {
-        // Client error - only support POST
-        res.status(405).end();
-    }
-});
-
-exports.uploadTest = functions.https.onRequest((req, res) => {
     if (req.method === 'POST') {
         const busboy = new Busboy({ headers: req.headers });
 
@@ -199,21 +116,13 @@ exports.uploadTest = functions.https.onRequest((req, res) => {
                     fs.unlinkSync(file);
 
                     const file_uploaded = bucket.file('images/'+ uid_global +'/' + filename_global);
-
-
-                    file_uploaded.getMetadata().then(results =>{
-                        const metadata = results[0];
-                        console.log('metadata: ', metadata);
-                    });
-
-
                     return file_uploaded.getSignedUrl({
                         action: 'read',
                         expires: '03-09-2491'
                     })
                 }).then(url => {
 
-                    console.log(url[0]);
+                    console.log('public download url: ', url[0]);
                     sendNotification(uid_global, url[0]);
                 }).catch(error => {
                     console.error(error);
